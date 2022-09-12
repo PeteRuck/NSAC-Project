09d242ee73b80cfbc6d3e22709ac78327c218ea7
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-12 21:38:43 +0100

    PR: Added get req

--- src/main/java/com/qa/main/controllers/AnimalController.java
+++ src/main/java/com/qa/main/controllers/AnimalController.java
@@ -1,7 +1,11 @@
 package com.qa.main.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +25,24 @@ public class AnimalController {
 			this.service = service;
 	}
 	
-	//	Post Requests (CREATE)
 	@PostMapping("/create")
 	public ResponseEntity<Animal> create(@RequestBody Animal input) {
 			return new ResponseEntity <Animal>(service.create(input), HttpStatus.CREATED);
 	}
 	
+	@GetMapping("/getAll") 
+	public List<Animal> getAll() {
+			return service.getAll();
+	}
+	
+	@GetMapping("/getById/{id}")
+	public Animal getById(@PathVariable long id) {
+			return service.getById(id);
+	}
+	
+	@GetMapping("/getByTypeGenus/{typeGenus}")
+	public List<Animal> getByTypeGenus(@PathVariable String typeGenus) {
+			return service.getByTypeGenus(typeGenus);
+	}
+			
 }
