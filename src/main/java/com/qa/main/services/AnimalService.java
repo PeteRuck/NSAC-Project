b7e7d8b52e447fc30be37e43b6b814b812a6558f
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-13 00:35:29 +0100

    PR: Added getter

--- src/main/java/com/qa/main/services/AnimalService.java
+++ src/main/java/com/qa/main/services/AnimalService.java
@@ -2,9 +2,23 @@ package com.qa.main.services;
 
 import org.springframework.stereotype.Service;
 
+import com.qa.main.entities.Animal;
+import com.qa.main.repos.AnimalRepo;
+
 @Service
 public class AnimalService {
 
+	private AnimalRepo repo;
+
+	public AnimalService(AnimalRepo repo) {
+		super();
+		this.repo = repo;
+	}
+
+	public Animal create(Animal input) {
+		return repo.saveAndFlush(input);
+	}
+	
 	
 	
 }
