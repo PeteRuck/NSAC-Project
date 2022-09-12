c4d35a2877e7ba7bd3ec6f7ff5c63518430b3c3a
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-12 13:10:41 +0100

    AL: init commit

---
+++ src/main/java/com/qa/main/Bae15SpringNsacp1Application.java
@@ -0,0 +1,13 @@
+package com.qa.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bae15SpringNsacp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bae15SpringNsacp1Application.class, args);
+	}
+
+}
