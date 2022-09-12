d915b4be00efbbf7c22809d457b7862352d56a8c
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-12 20:38:43 +0100

    PR: Added Override/Impliment

--- src/main/java/com/qa/main/entities/Animal.java
+++ src/main/java/com/qa/main/entities/Animal.java
@@ -1,5 +1,7 @@
 package com.qa.main.entities;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -85,7 +87,22 @@ public class Animal {
 	public void setTypeGenus(String typeGenus) {
 		this.typeGenus = typeGenus;
 	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(commonName, genus, id, size, typeGenus);
+	}
 
-	
-	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		return Objects.equals(commonName, other.commonName) && Objects.equals(genus, other.genus) && id == other.id
+				&& Float.floatToIntBits(size) == Float.floatToIntBits(other.size)
+				&& Objects.equals(typeGenus, other.typeGenus);
+	}	
 }
