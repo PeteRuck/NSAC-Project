b45c6a923454c6f985ad7d4479715180577345bc
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-12 21:46:29 +0100

    PR: Updated put req

--- src/main/java/com/qa/main/controllers/AnimalController.java
+++ src/main/java/com/qa/main/controllers/AnimalController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +51,10 @@ public class AnimalController {
 			return service.getByCommonNameContaining(commonName);
 	}
 	
-			
+	@PutMapping("/update/{id}")
+	public Animal update(@PathVariable long id, @RequestBody Animal input) {
+			return service.update(id, input);
+	}
+	
+	
 }
