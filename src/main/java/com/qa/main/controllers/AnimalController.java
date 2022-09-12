a0db093af02fbea10442a27240ada410eb397880
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-12 21:49:52 +0100

    PR: Added delete req

--- src/main/java/com/qa/main/controllers/AnimalController.java
+++ src/main/java/com/qa/main/controllers/AnimalController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,5 +57,8 @@ public class AnimalController {
 			return service.update(id, input);
 	}
 	
-	
+	@DeleteMapping("/delete/{id}")
+	public boolean delete(@PathVariable long id) {
+			return service.delete(id);
+	}
 }
