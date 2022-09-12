ab3798013ed3f1182ecc3d1b3cc9301923380292
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-12 20:27:42 +0100

    PR: Entity update

--- src/main/java/com/qa/main/entities/Animal.java
+++ src/main/java/com/qa/main/entities/Animal.java
@@ -1,5 +1,33 @@
 package com.qa.main.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Animal {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String commonName;
+	
+	@Column(nullable = false)
+	private String genus;
+	
+	@Column(nullable = false)
+	private float size;
+	
+	@Column(nullable = false)
+	private String typeGenus;
+	
+	public Animal() {
+        super();
+    }
+	
+	
 }
