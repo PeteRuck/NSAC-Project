7b7b78149b79adb35b3c987ac921215772e961ad
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-12 21:36:47 +0100

    PR: Added post req

--- src/main/java/com/qa/main/controllers/AnimalController.java
+++ src/main/java/com/qa/main/controllers/AnimalController.java
@@ -1,5 +1,9 @@
 package com.qa.main.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,16 @@ import com.qa.main.services.AnimalService;
 public class AnimalController {
 
 		private AnimalService service;
+	
+		public AnimalController(AnimalService service) {
+			super();
+			this.service = service;
+	}
+	
+	//	Post Requests (CREATE)
+	@PostMapping("/create")
+	public ResponseEntity<Animal> create(@RequestBody Animal input) {
+			return new ResponseEntity <Animal>(service.create(input), HttpStatus.CREATED);
+	}
 	
 }
