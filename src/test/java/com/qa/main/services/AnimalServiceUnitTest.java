613a55da2e2547311dccf20d8606f3f698930d12
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-14 00:07:41 +0100

    PR: added get tests

--- src/test/java/com/qa/main/services/AnimalServiceUnitTest.java
+++ src/test/java/com/qa/main/services/AnimalServiceUnitTest.java
@@ -2,6 +2,10 @@ package com.qa.main.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +38,47 @@ public class AnimalServiceUnitTest {
 		assertEquals(result, service.create(input));
 	}
 	
+	@Test
+	public void getAllTest() {
+		List<Animal> result = new ArrayList<>();
+		result.add(new Animal(1L, "Fox", "Vulpes", 50, "Canidae"));
+		
+		
+		Mockito.when(repo.findAll()).thenReturn(result);
+		
+		assertEquals(result, service.getAll());
+	}
 	
+	@Test
+	public void getByIdTest() {
+		Optional<Animal> OptionalOutput = Optional.of(new Animal(1L,"Fox", "Vulpes", 50, "Canidae"));
+		Animal output = new Animal(1L, "Fox", "Vulpes", 50, "Canidae");
+	
+	
+		Mockito.when(repo.findById(1L)).thenReturn(OptionalOutput);
+		
+		assertEquals(output, service.getById(1L));
+	}
+		
+	@Test
+	public void getByTypeGenusTest() {
+		List<Animal> result = new ArrayList<>();
+		result.add(new Animal("Fox", "Vulpes", 50, "Canidae"));
+		
+		Mockito.when(repo.findAnimalByTypeGenus("Canidae")).thenReturn(result);
+		
+		assertEquals(result, service.getByTypeGenus("Canidae"));
+	}
+		
+	@Test
+	public void getByCommonNameContaining() {
+		List<Animal> result = new ArrayList<>();
+		result.add(new Animal("Fox", "Vulpes", 50, "Canidae"));
+		
+		Mockito.when(repo.findByCommonNameContaining("Fox")).thenReturn(result);
+		
+		assertEquals(result, service.getByCommonNameContaining("Fox"));
+	}
+		
 	
 }
