2ba55e61bee636ada82a4e3ca6f9a422709eb823
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-13 20:49:07 +0100

    PR: Updated AnimalTest

--- src/test/java/com/qa/main/entities/AnimalTest.java
+++ src/test/java/com/qa/main/entities/AnimalTest.java
@@ -1,5 +1,34 @@
 package com.qa.main.entities;
 
-public class AnimalTest {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AnimalTest {
+	
+	@Test
+	public void equalsNullTest() {
+		Animal animal = new Animal("Fox", "Vulpes", 50, "Canidae");
+		
+		assertFalse(animal.equals(null));
+	}
+	
+	@Test
+	public void differentClassTest() {
+		Animal animal = new Animal("Fox", "Vulpes", 50, "Canidae");
+		String testString = "Hi";
+		
+		assertFalse(animal.equals(testString));
+	}
+	
+	@Test
+	public void checkHashTest() {
+		Animal one = new Animal("Fox", "Vulpes", 50, "Canidae");
+		Animal two = new Animal("Fox", "Vulpes", 50, "Canidae");
+		
+		assertTrue(one.hashCode() == two.hashCode());
+	}	
 }
