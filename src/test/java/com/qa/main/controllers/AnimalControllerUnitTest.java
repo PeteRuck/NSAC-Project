7538237267a2058667448fb49ade6a23dc9b9f3d
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-13 22:37:30 +0100

    PR: added create test

--- src/test/java/com/qa/main/controllers/AnimalControllerUnitTest.java
+++ src/test/java/com/qa/main/controllers/AnimalControllerUnitTest.java
@@ -1,25 +1,49 @@
 package com.qa.main.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.main.entities.Animal;
 import com.qa.main.services.AnimalService;
 
 @WebMvcTest
 public class AnimalControllerUnitTest {
 
 	@Autowired
-	private MockMvc mvc; // This is for sending mock requests
+	private MockMvc mvc; 
 	
 	@Autowired
-	private ObjectMapper mapper; // Is for converting TO and FROM JSON data
+	private ObjectMapper mapper;
 	
 	@MockBean
 	private AnimalService service;
+	
+	@Test
+	public void createTest() throws Exception {
+		Animal input = new Animal("Fox", "Vulpes", 50, "Canidae");
+		String inputAsJSON = mapper.writeValueAsString(input);
 		
+		Animal response = new Animal(2L, "Fox", "Vulpes", 50, "Canidae");
+		String responseAsJSON = mapper.writeValueAsString(response);
+		
+		Mockito.when(service.create(input)).thenReturn(response); 
+		
+		mvc.perform(post("/animal/create")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(inputAsJSON))
+			.andExpect(status().isCreated())
+			.andExpect(content().json(responseAsJSON));
+	}
 	
 	
 	
