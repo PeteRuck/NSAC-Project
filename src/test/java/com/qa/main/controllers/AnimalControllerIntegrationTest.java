0b2d076765b7b4487907038a0bbdd3783bc43227
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-13 21:21:00 +0100

    PR: Added Autowired

--- src/test/java/com/qa/main/controllers/AnimalControllerIntegrationTest.java
+++ src/test/java/com/qa/main/controllers/AnimalControllerIntegrationTest.java
@@ -1,18 +1,27 @@
 package com.qa.main.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
-@AutoConfigureMockMvc // Allows us to send mock requests using MockMvc
+@AutoConfigureMockMvc
 @Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
-@ActiveProfiles("test") // Switch to the test profile (H2 Database)
+@ActiveProfiles("test")
 public class AnimalControllerIntegrationTest {
 
-
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
 
 
 	
