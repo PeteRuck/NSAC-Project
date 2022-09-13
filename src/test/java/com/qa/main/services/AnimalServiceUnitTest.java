966aafe8a075941e86e9aa0d62a6f4d3d190d54e
Pete Ruck <ipslore@hotmail.co.uk>
2022-09-13 23:48:37 +0100

    PR: added create test

--- src/test/java/com/qa/main/services/AnimalServiceUnitTest.java
+++ src/test/java/com/qa/main/services/AnimalServiceUnitTest.java
@@ -1,9 +1,14 @@
 package com.qa.main.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.qa.main.entities.Animal;
 import com.qa.main.repos.AnimalRepo;
 
 @SpringBootTest
@@ -15,6 +20,20 @@ public class AnimalServiceUnitTest {
 	@MockBean
 	private AnimalRepo repo;
 		
+	@Test
+	public void createTest() {
+		// Create an object for saving
+		Animal input = new Animal("Fox", "Vulpes", 50, "Canidae");
+		
+		// Create an object for the result
+		Animal result = new Animal(2L, "Fox", "Vulpes", 50, "Canidae");
+		
+		Mockito.when(repo.saveAndFlush(input)).thenReturn(result);
+		
+		// assertEquals(expected result, actual result)
+		assertEquals(result, service.create(input));
+	}
+	
 	
 	
 }
